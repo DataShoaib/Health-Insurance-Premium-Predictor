dfff669294cacc4fff2f439a3b8e585b968ccbf6
datashoaib <mdshoaiba478@gmail.com>
2026-03-01 16:55:40 +0530

    error fixed

--- tests/validation/test_model_validation.py
+++ tests/validation/test_model_validation.py
@@ -4,9 +4,9 @@ from sklearn.metrics import mean_absolute_error,r2_score,root_mean_squared_error
 def test_model_performance_and_comparing_with_prod(staging_model,production_model,x_test_data,y_test_data):
     y_pred=staging_model.predict(x_test_data)
 
-    staging_mae=mean_absolute_error(y_pred,y_test_data)
-    staging_r2=r2_score(y_pred,y_test_data)
-    staging_rmse=root_mean_squared_error(y_pred,y_test_data)
+    staging_mae=mean_absolute_error(y_test_data,y_pred)
+    staging_r2=r2_score(y_test_data,y_pred)
+    staging_rmse=root_mean_squared_error(y_test_data,y_pred)
 
     assert staging_mae >= 0
     assert staging_rmse >= 0
@@ -15,9 +15,9 @@ def test_model_performance_and_comparing_with_prod(staging_model,production_mode
     if production_model:
 
         prod_pred=production_model.predict(x_test_data)
-        production_mae=mean_absolute_error(prod_pred,y_test_data)
-        production_rmse=root_mean_squared_error(prod_pred,y_test_data)
-        production_r2_scr=r2_score(prod_pred,y_test_data)
+        production_mae=mean_absolute_error(y_test_data,prod_pred)
+        production_rmse=root_mean_squared_error(y_test_data,prod_pred)
+        production_r2_scr=r2_score(y_test_data,prod_pred)
 
         assert staging_mae < production_mae
         assert staging_rmse < production_rmse
