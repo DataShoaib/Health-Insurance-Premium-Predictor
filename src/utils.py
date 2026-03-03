4ce49a8fc9eb082a85c71b04a513d0bfedc73e37
datashoaib <mdshoaiba478@gmail.com>
2026-03-03 17:06:10 +0530

    change utils

--- src/utils.py
+++ src/utils.py
@@ -7,19 +7,18 @@ load_dotenv()
 
 def setup_mlflow():
 
-    dagshub_token = os.getenv("DAGSHUB_TRACKING_PASSWORD")
-    tracking_uri = 'https://dagshub.com/DataShoaib/Health-Insurance-Premium-predictor.mlflow'
+    username = os.getenv("MLFLOW_TRACKING_USERNAME")
+    password = os.getenv("MLFLOW_TRACKING_PASSWORD")
+    tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
 
-    if not dagshub_token:
-        raise EnvironmentError("DAGSHUB_TRACKING_PASSWORD is not set")
+    if not all([username, password, tracking_uri]):
+        print("MLflow credentials missing. Skipping setup.")
+        return False
 
+    os.environ["MLFLOW_TRACKING_USERNAME"] = username
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = password
 
-    # set credentials first
-    os.environ["MLFLOW_TRACKING_USERNAME"] = "DataShoaib"
-    os.environ["DAGSHUB_TRACKING_PASSWORD"] = dagshub_token
-
-    # set tracking URI
     mlflow.set_tracking_uri(tracking_uri)
-
-   
     mlflow.set_registry_uri(tracking_uri)
+
+    return True
