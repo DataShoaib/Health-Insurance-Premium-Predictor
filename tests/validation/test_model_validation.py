3b366eefbe5b5f6c073fdc4f862ccc10111270de
datashoaib <mdshoaiba478@gmail.com>
2026-03-06 16:05:36 +0530

    validatopn file change

--- tests/validation/test_model_validation.py
+++ tests/validation/test_model_validation.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import mean_absolute_error,r2_score,root_mean_squared_error
 
+TOLERANCE = 0.05
 
 def test_model_performance_and_comparing_with_prod(staging_model,production_model,x_test_data,y_test_data):
     y_pred=staging_model.predict(x_test_data)
@@ -19,6 +20,6 @@ def test_model_performance_and_comparing_with_prod(staging_model,production_mode
         production_rmse=root_mean_squared_error(y_test_data,prod_pred)
         production_r2_scr=r2_score(y_test_data,prod_pred)
 
-        assert staging_mae < production_mae
-        assert staging_rmse < production_rmse
-        assert staging_r2 > production_r2_scr
+        assert staging_mae <= production_mae * (1 + TOLERANCE)
+        assert staging_rmse <= production_rmse * (1 + TOLERANCE)
+        assert staging_r2 >= production_r2_scr * (1 - TOLERANCE)
